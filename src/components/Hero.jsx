35e7c074ecdcb169ab6a1825087c562ed9561520
JoeTheorium <fernandoandresraggio@gmail.com>
2023-08-23 13:09:20 -0300

    update Hero and UnderConstruction styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,10 @@ import Carousel from '../components/Carousel';
 
 function Hero() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen px-10 text-center">
-      <section className="bg-blue-900 text-white p-8 rounded-lg mb-8 max-w-2xl">
-        <div className="flex items-start mb-4">
-          <h3 className="text-xl font-semibold">Find your perfect trip,</h3>
-        </div>
-        <div className="flex items-end">
-          <h3 className="text-xl font-semibold">designed by insiders who know and love their cities!</h3>
-        </div>
+    <main className="flex flex-col items-center justify-center min-h-screen px-10 text-center bg-blue-900 text-white">
+      <section className="p-8 rounded-lg mb-8 max-w-2xl bg-opacity-75">
+        <h3 className="text-xl font-semibold mb-2">Find your perfect trip,</h3>
+        <h3 className="text-xl font-semibold">designed by insiders who know and love their cities!</h3>
         <button className="bg-blue-500 text-white px-6 py-3 rounded-full mt-4 hover:bg-blue-600 hover:shadow-md transition duration-300 ease-in-out">View More</button>
       </section>
       <Carousel>
--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -3,20 +3,20 @@ import {NavLink} from "react-router-dom";
 
 const UnderConstruction = () => {
   return (
-    <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-      <section style={{textAlign: 'center', marginBottom: '2rem'}}>
-        <h1>Page Under Construction</h1>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <section className="text-center mb-8">
+        <h1 className="text-3xl font-bold">Page Under Construction</h1>
         <h3>We´re working on bringing you new content</h3>
         <p>Check back later for updates!</p>
-        <NavLink to="/" style={{textDecoration: 'none', color: '#333'}}>
-          <button style={{marginTop: '1rem', padding: '0.5rem 1rem', borderRadius: '4px', border: '1px solid #333', backgroundColor: 'transparent', cursor: 'pointer'}}>
+        <NavLink to="/" className="mt-4 text-gray-600">
+          <button className="mt-4 py-2 px-4 border border-gray-600 rounded cursor-pointer">
             Back to Home
           </button>
         </NavLink>
       </section>
-      <aside style={{minHeight: '10vh', height: '40vh', display: 'flex', justifyContent: 'center'}}>
-        <div style={{border: '2px dashed #888', padding: '1rem', borderRadius: '8px'}}>
-          <p>AÑADIR IMAGEN!!!</p>
+      <aside className="flex justify-center h-40">
+        <div className="p-4 rounded-lg">
+          <img src="./src/assets/underConstruction.jpg" alt="PAGE UNDER CONSTRUCTION!" className="w-96" />
         </div>
       </aside>
     </main>
