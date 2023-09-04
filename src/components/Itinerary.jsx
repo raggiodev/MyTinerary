9981cf25dbf775314c0b7954cc3b97a670621a67
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-04 19:30:56 -0300

    add best styles to Itinerary component

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -5,7 +5,7 @@ import underConstruction from "../assets/underConstruction.jpg";
 
 const Itinerary = ({ data }) => {
   if (!data) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   const [expanded, setExpanded] = useState(false);
@@ -23,12 +23,14 @@ const Itinerary = ({ data }) => {
 
   const likeIcon = isLiked ? <FaHeart /> : <FaRegHeart />;
 
-  const renderDollarIcon = () => {
-    const dollarIcon = [];
+  const renderDollarIcons = () => {
+    const dollarIcons = [];
     for (let i = 0; i < data.price; i++) {
-      dollarIcon.push(<FaMoneyBillWave key={i} className="w-4 h-4 inline-block" />);
+      dollarIcons.push(
+        <FaMoneyBillWave key={i} className="w-4 h-4 inline-block" />
+      );
     }
-    return dollarIcon;
+    return dollarIcons;
   };
 
   return (
@@ -39,13 +41,10 @@ const Itinerary = ({ data }) => {
       >
         <div className="absolute top-2 right-2">
           <div
-            className={`${
-              isLiked ? "" : ""
-            }`}
+            className={`${isLiked ? "text-red-500" : ""} cursor-pointer`}
             onClick={toggleLike}
           >
-            {likeIcon}
-            {likes}
+            {likeIcon} {likes}
           </div>
         </div>
       </div>
@@ -56,37 +55,49 @@ const Itinerary = ({ data }) => {
         ))}
       </div>
       <div className="p-2">
-        <div>
-          <img src={data.authorPhoto} alt={data.authorName} />
+        <div className="flex items-center">
+          <img
+            src={data.authorPhoto}
+            alt={data.authorName}
+            className="w-12 h-12 rounded-full mr-2"
+          />
           <h3>{data.authorName}</h3>
         </div>
         <h5>
-          <span>Price: </span>
-          {renderDollarIcon()}
+          <span className="font-semibold">Price:</span> {renderDollarIcons()}
         </h5>
         <h5>
-          <span>Duration: </span>
-          {data.duration} min
+          <span className="font-semibold">Duration:</span> {data.duration} min
         </h5>
       </div>
-      <div className={expanded ? "expanded" : "collapsed expanded"}>
-        <div>
-          <h2>
-            Section <span>Under </span>Construction
+      <div className="p-2">
+        <div
+          className={`${
+            expanded ? "opacity-100 max-h-96" : "opacity-0 max-h-0"
+          } transition-all duration-500 ease-in-out overflow-hidden`}
+        >
+          <h2 className="mb-2">
+            Section <span>Under</span> Construction
           </h2>
           <h5>Content will be available soon</h5>
+          <img
+            style={{ maxWidth: "90%", maxHeight: "10rem" }}
+            src={underConstruction}
+            alt="workers construction house"
+          />
         </div>
-        <img
-          style={{ maxWidth: "90%", maxHeight: "10rem" }}
-          src={underConstruction}
-          alt="workers construction house"
-        />
       </div>
       <div
         onClick={() => setExpanded(!expanded)}
         className="cursor-pointer p-2 text-blue-500 hover:underline"
       >
-        View More <BsChevronDown size="1rem" style={{ transition: "1s ease", transform: expanded ? 'rotate(-180deg)' : '' }} />
+        View More{" "}
+        <BsChevronDown
+          size="1rem"
+          className={`${
+            expanded ? "transform rotate-180" : ""
+          } transition-transform duration-500 ease-in-out`}
+        />
       </div>
     </div>
   );
