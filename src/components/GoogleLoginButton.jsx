dcce33cf1a3399629494c6fff72733ccae6baeef
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-13 13:39:04 -0300

    login with google

---
+++ src/components/GoogleLoginButton.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {useGoogleLogin} from "@react-oauth/google";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
+
+const GoogleLoginButton = ({fn}) => {
+  const login = useGoogleLogin({
+
+    onSuccess: async tokenResponse => {
+      console.log(tokenResponse)
+      // const infoUser = jwtDecode(tokenResponse.credential)
+      const {data} = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: "Bearer " + tokenResponse.access_token
+        }
+      })
+      fn({
+        email: data.email,
+        name: data.given_name + " " + data.family_name,
+        password: "Aa_123",
+        terms: true
+      })
+      console.log(data);
+    },
+
+  })
+
+  return (
+    <div onClick={() => login()} className="w-3/12 max-w-full px-1 mr-auto flex-0">
+      <a className="flex justify-center w-full px-6 py-3 mb-4 font-bold text-center text-gray-200 uppercase align-middle transition-all bg-transparent border border-gray-200 border-solid rounded-lg shadow-none cursor-pointer hover:scale-102 leading-pro text-xs ease-soft-in tracking-tight-soft bg-150 bg-x-25 hover:bg-transparent hover:opacity-75">
+        <svg
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          viewBox="0 0 64 64"
+          height="32px"
+          width="24px"
+        >
+          <g fillRule="evenodd" fill="none" strokeWidth="1" stroke="none">
+            <g fillRule="nonzero" transform="translate(3.000000, 2.000000)">
+              <path
+                fill="#4285F4"
+                d="M57.8123233,30.1515267 C57.8123233,27.7263183 57.6155321,25.9565533 57.1896408,24.1212666 L29.4960833,24.1212666 L29.4960833,35.0674653 L45.7515771,35.0674653 C45.4239683,37.7877475 43.6542033,41.8844383 39.7213169,44.6372555 L39.6661883,45.0037254 L48.4223791,51.7870338 L49.0290201,51.8475849 C54.6004021,46.7020943 57.8123233,39.1313952 57.8123233,30.1515267"
+              ></path>
+              <path
+                fill="#34A853"
+                d="M29.4960833,58.9921667 C37.4599129,58.9921667 44.1456164,56.3701671 49.0290201,51.8475849 L39.7213169,44.6372555 C37.2305867,46.3742596 33.887622,47.5868638 29.4960833,47.5868638 C21.6960582,47.5868638 15.0758763,42.4415991 12.7159637,35.3297782 L12.3700541,35.3591501 L3.26524241,42.4054492 L3.14617358,42.736447 C7.9965904,52.3717589 17.959737,58.9921667 29.4960833,58.9921667"
+              ></path>
+              <path
+                fill="#FBBC05"
+                d="M12.7159637,35.3297782 C12.0932812,33.4944915 11.7329116,31.5279353 11.7329116,29.4960833 C11.7329116,27.4640054 12.0932812,25.4976752 12.6832029,23.6623884 L12.6667095,23.2715173 L3.44779955,16.1120237 L3.14617358,16.2554937 C1.14708246,20.2539019 0,24.7439491 0,29.4960833 C0,34.2482175 1.14708246,38.7380388 3.14617358,42.736447 L12.7159637,35.3297782"
+              ></path>
+              <path
+                fill="#EB4335"
+                d="M29.4960833,11.4050769 C35.0347044,11.4050769 38.7707997,13.7975244 40.9011602,15.7968415 L49.2255853,7.66898166 C44.1130815,2.91684746 37.4599129,0 29.4960833,0 C17.959737,0 7.9965904,6.62018183 3.14617358,16.2554937 L12.6832029,23.6623884 C15.0758763,16.5505675 21.6960582,11.4050769 29.4960833,11.4050769"
+              ></path>
+            </g>
+          </g>
+        </svg>
+      </a>
+    </div>
+  );
+};
+
+export default GoogleLoginButton;
--- src/main.jsx
+++ src/main.jsx
@@ -27,3 +27,31 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* </Provider> */}
   </React.StrictMode>
 );
+
+// // import React from "react";
+// import ReactDOM from "react-dom/client";
+// import {GoogleOAuthProvider} from "@react-oauth/google";
+// import {ToastContainer} from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import {Provider} from "react-redux";
+// import {store} from "./redux/store.js";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <GoogleOAuthProvider clientId={import.meta.env["VITE_GOOGLE_ID"]}>
+//       <App />
+//       <ToastContainer
+//         position="top-right"
+//         hideProgressBar={false}
+//         newestOnTop={false}
+//         closeOnClick
+//         pauseOnFocusLoss
+//         draggable
+//         pauseOnHover
+//         theme="dark"
+//       />
+//     </GoogleOAuthProvider>
+//   </Provider>
+// );
