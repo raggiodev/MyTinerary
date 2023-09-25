c9ef8529943a3e353d43f9c3b24eefe1883adf79
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-25 19:46:39 -0300

    Install analytics of Vercel

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -67,7 +67,7 @@ const Details = () => {
         {itineraries.length === 0 ? (
           <NoItinerariesFound />
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {itineraries.map((itinerary, index) => {
               return <Itinerary data={itinerary} key={index} />;
             })}
