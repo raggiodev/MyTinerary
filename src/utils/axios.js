479c18a2b5c0d91233b7623045f162636b5edf9d
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-25 23:48:09 -0300

    conectando api online con frontend online en Vercel

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const server = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
