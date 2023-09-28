e63b88126cd3d4622a9921b0c004f4dfd1445fb9
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-28 12:15:51 -0300

    NoItinerariesFound component corrected

--- src/components/NoItinerariesFound.jsx
+++ src/components/NoItinerariesFound.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
 import {FaSearch} from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
 
 const NoItinerariesFound = () => {
   const navigate = useNavigate();
@@ -10,13 +9,13 @@ const NoItinerariesFound = () => {
       <section className="p-8 rounded-lg mb-8 max-w-2xl bg-opacity-75">
         <h3 className="text-xl font-semibold mb-2">No Itineraries Found</h3>
         <h3 className="text-xl font-semibold">
-          Oops... There are no itineraries available for this city right now.
+          Ooh sh... There are no itineraries available for this city right now.
         </h3>
         <button
           onClick={() => navigate(-1)}
           className="bg-blue-500 text-white px-6 py-3 rounded-full mt-4 hover:bg-blue-600 hover:shadow-md transition duration-300 ease-in-out"
         >
-          Go Back to Cities
+          Back to Cities
         </button>
       </section>
       <aside>
